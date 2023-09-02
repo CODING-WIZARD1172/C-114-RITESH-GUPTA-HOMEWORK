9093e1a5064af3b41198a9854acc87bc9f3cb1b4
CODING-WIZARD1172 <124650826+CODING-WIZARD1172@users.noreply.github.com>
2023-09-02 18:27:46 +0530

    Add files via upload

---
+++ C-114/test1.py
@@ -0,0 +1,7 @@
+def my_function(fname):
+    print(fname + ' refnes')
+
+
+my_function('emil')
+my_function('tobias')
+my_function('linux')
---
+++ C-114/test2.py
@@ -0,0 +1,10 @@
+a = 33
+# b='33'
+b = 33
+
+if b > a:
+    print('b is greater than a')
+elif b == a:
+    print('b= a')
+else:
+    print('b is smaller than a')
